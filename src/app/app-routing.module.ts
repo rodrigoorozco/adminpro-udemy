d7bdc3f80b36253e5869816bdd5de12f1aaa23c4
Rodrigo <rodrigo.orozco94@gmail.com>
2020-06-05 16:55:46 -0500

    Modificaciones en los mudulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
-import { ProgessComponent } from "./pages/progess/progess.component";
-import { Charts1Component } from "./pages/charts1/charts1.component";
 import { NoPageFoundComponent } from "./shared/no-page-found/no-page-found.component";
-import { PagesComponent } from "./pages/pages.component";
 import { RegisterComponent } from './login/register.component';
 
-const routes: Routes = [
-  {
-    path: "",
-    component: PagesComponent,
-    children: [
-      { path: "dashboard", component: DashboardComponent },
-      { path: "progress", component: ProgessComponent },
-      { path: "charts1", component: Charts1Component },
-      { path: "", redirectTo: "/dashboard", pathMatch: "full" },
-    ],
-  },
+const routes: Routes = [  
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "**", component: NoPageFoundComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export const APP_ROUTES = RouterModule.forRoot(routes, {useHash:true})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgessComponent } from './pages/progess/progess.component';
-import { Charts1Component } from './pages/charts1/charts1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import {PagesModule} from './pages/pages.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NoPageFoundComponent,
-    DashboardComponent,
-    ProgessComponent,
-    Charts1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
